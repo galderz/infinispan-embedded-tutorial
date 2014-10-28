cb1219330d3b1ef7e64981bbeb9435a14d8adcad
Tristan Tarrant <ttarrant@redhat.com>
2014-10-28 14:24:29 +0100

    step-2/expiration

--- src/main/java/org/infinispan/tutorial/embedded/EmbeddedTutorial.java
+++ src/main/java/org/infinispan/tutorial/embedded/EmbeddedTutorial.java
@@ -1,11 +1,13 @@
 package org.infinispan.tutorial.embedded;
 
+import java.util.concurrent.TimeUnit;
+
 import org.infinispan.Cache;
 import org.infinispan.manager.DefaultCacheManager;
 
 public class EmbeddedTutorial {
 
-   public static void main(String[] args) {
+   public static void main(String[] args) throws InterruptedException {
       // Create a default local cache manager
       DefaultCacheManager cacheManager = new DefaultCacheManager();
 
@@ -20,6 +22,19 @@ public class EmbeddedTutorial {
 
       // Print it out
       System.out.printf("key ==> %s\n", value);
+
+      // Insert a mortal entry, i.e. an entry which will expire
+      cache.put("mortal", "I am going to die", 1, TimeUnit.SECONDS);
+
+      // Print it out
+      System.out.printf("mortal ==> %s\n", cache.get("mortal"));
+
+      // Let's wait a couple of seconds
+      System.out.println("Sleeping for 2 seconds...");
+      Thread.sleep(TimeUnit.SECONDS.toMillis(2));
+
+      // Print it out again. It will have died
+      System.out.printf("mortal ==> %s\n", cache.get("mortal"));
 
       // Stop the cache manager and release all resources
       cacheManager.stop();
