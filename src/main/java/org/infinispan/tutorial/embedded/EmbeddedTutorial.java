04033be0cd3478cd637b9faddf08a79164dcb8e2
Tristan Tarrant <ttarrant@redhat.com>
2014-10-28 14:15:20 +0100

    step-1/cache-reading-writing

--- src/main/java/org/infinispan/tutorial/embedded/EmbeddedTutorial.java
+++ src/main/java/org/infinispan/tutorial/embedded/EmbeddedTutorial.java
@@ -1,5 +1,6 @@
 package org.infinispan.tutorial.embedded;
 
+import org.infinispan.Cache;
 import org.infinispan.manager.DefaultCacheManager;
 
 public class EmbeddedTutorial {
@@ -7,6 +8,18 @@ public class EmbeddedTutorial {
    public static void main(String[] args) {
       // Create a default local cache manager
       DefaultCacheManager cacheManager = new DefaultCacheManager();
+
+      // Obtain the default cache
+      Cache<String, String> cache = cacheManager.getCache();
+
+      // Insert an entry into the cache
+      cache.put("key", "value");
+
+      // Read an entry from the cache
+      String value = cache.get("key");
+
+      // Print it out
+      System.out.printf("key ==> %s\n", value);
 
       // Stop the cache manager and release all resources
       cacheManager.stop();
