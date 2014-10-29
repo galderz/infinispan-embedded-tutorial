7aaa599213ab847fb07a6434251369ab3668a77e
Tristan Tarrant <ttarrant@redhat.com>
2014-10-29 14:34:21 +0100

    step-3/listeners

--- src/main/java/org/infinispan/tutorial/embedded/EmbeddedTutorial.java
+++ src/main/java/org/infinispan/tutorial/embedded/EmbeddedTutorial.java
@@ -4,8 +4,21 @@ import java.util.concurrent.TimeUnit;
 
 import org.infinispan.Cache;
 import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.notifications.Listener;
+import org.infinispan.notifications.cachelistener.annotation.CacheEntryCreated;
+import org.infinispan.notifications.cachelistener.event.CacheEntryCreatedEvent;
 
 public class EmbeddedTutorial {
+
+   @Listener
+   public static class SimpleListener {
+      @CacheEntryCreated
+      public void modifiedEntry(CacheEntryCreatedEvent<String, String> event) {
+         if (!event.isPre()) {
+            System.out.printf("** Key '%s' was created\n", event.getKey());
+         }
+      }
+   }
 
    public static void main(String[] args) throws InterruptedException {
       // Create a default local cache manager
@@ -13,6 +26,9 @@ public class EmbeddedTutorial {
 
       // Obtain the default cache
       Cache<String, String> cache = cacheManager.getCache();
+
+      // Add the listener to the cache
+      cache.addListener(new SimpleListener());
 
       // Insert an entry into the cache
       cache.put("key", "value");
