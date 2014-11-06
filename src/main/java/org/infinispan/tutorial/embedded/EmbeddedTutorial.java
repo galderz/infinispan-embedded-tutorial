80fb5c225a1a9b0dd483d7800390406b806ae365
Tristan Tarrant <ttarrant@redhat.com>
2014-11-06 12:25:45 +0100

    step-4/clustering

--- src/main/java/org/infinispan/tutorial/embedded/EmbeddedTutorial.java
+++ src/main/java/org/infinispan/tutorial/embedded/EmbeddedTutorial.java
@@ -1,12 +1,18 @@
 package org.infinispan.tutorial.embedded;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import org.infinispan.Cache;
+import org.infinispan.configuration.cache.CacheMode;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.global.GlobalConfigurationBuilder;
 import org.infinispan.manager.DefaultCacheManager;
 import org.infinispan.notifications.Listener;
 import org.infinispan.notifications.cachelistener.annotation.CacheEntryCreated;
 import org.infinispan.notifications.cachelistener.event.CacheEntryCreatedEvent;
+import org.infinispan.notifications.cachemanagerlistener.annotation.ViewChanged;
+import org.infinispan.notifications.cachemanagerlistener.event.ViewChangedEvent;
 
 public class EmbeddedTutorial {
 
@@ -20,12 +26,40 @@ public class EmbeddedTutorial {
       }
    }
 
+   @Listener
+   public static class ClusterListener {
+      CountDownLatch latch = new CountDownLatch(1);
+
+      @ViewChanged
+      public void viewChanged(ViewChangedEvent event) {
+         System.out.printf("** View changed: %s\n", event.getNewMembers());
+         // Once we have two members we open release the latch
+         if (event.getCacheManager().getMembers().size() == 2) {
+            latch.countDown();
+         }
+      }
+   }
+
    public static void main(String[] args) throws InterruptedException {
-      // Create a default local cache manager
-      DefaultCacheManager cacheManager = new DefaultCacheManager();
+      // We want a clustered configuration
+      GlobalConfigurationBuilder global = GlobalConfigurationBuilder.defaultClusteredBuilder();
+
+      // Redefine the default cache to be a distributed, synchronous cache
+      ConfigurationBuilder builder = new ConfigurationBuilder();
+      builder.clustering().cacheMode(CacheMode.DIST_SYNC);
+
+      // Create a default clustered cache manager
+      DefaultCacheManager cacheManager = new DefaultCacheManager(global.build(), builder.build());
+
+      // Register a cluster listener
+      ClusterListener clusterListener = new ClusterListener();
+      cacheManager.addListener(clusterListener);
 
       // Obtain the default cache
       Cache<String, String> cache = cacheManager.getCache();
+
+      // Wait for all nodes to join
+      clusterListener.latch.await();
 
       // Add the listener to the cache
       cache.addListener(new SimpleListener());
